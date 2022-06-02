7d4e10ed27733ad61f8aa118964adfe35d8aec14
chenlianghong <clh021@gmail.com>
2022-06-02 08:13:09 +0800

    parser: func update in &pipe

--- pipes/title.go
+++ pipes/title.go
@@ -2,14 +2,14 @@ package pipes
 
 import "fmt"
 
-func (p PipeObj) TitleByStartWith(params []string, txt *[]string) error {
+func (p *PipeObj) TitleByStartWith(params []string, txt *[]string) error {
 	// res := make(map[string]string)
 	fmt.Printf("params: %+v \n", params)
 	// fmt.Printf("txt: %+v \n", txt)
 	return nil
 }
 
-func (p PipeObj) TitleByNextLineStartWith(params []string, txt *[]string) error {
+func (p *PipeObj) TitleByNextLineStartWith(params []string, txt *[]string) error {
 	fmt.Printf("params: %+v \n", params)
 	return nil
 }
