dc1b0904e3c26cad7f39a531325a2881843f0a06
chenlianghong <clh021@gmail.com>
2022-09-07 21:56:43 +0800

    add all func.go

--- pipes/func.go
+++ pipes/func.go
@@ -1,8 +1,24 @@
 package pipes
 
 import (
+	"fmt"
 	"strings"
 )
+
+func (p *PipeObj) SC(params []string) error {
+	fmt.Printf("params: %+v \n", params)
+	return nil
+}
+
+func (p *PipeObj) Join(params []string) error {
+	fmt.Printf("params: %+v \n", params)
+	return nil
+}
+
+func (p *PipeObj) Contain(params []string) error {
+	fmt.Printf("params: %+v \n", params)
+	return nil
+}
 
 func (p *PipeObj) Split(params []string) error {
 	p.lastArr = strings.Split(p.lastStr, params[0])
