186590fb27b25ad639b1213ac3bfb05d30d1a78e
chenlianghong <clh021@gmail.com>
2022-12-04 19:06:46 +0800

    refactor: add tui todo

--- cmd/main.go
+++ cmd/main.go
@@ -5,6 +5,7 @@ import (
 	"os"
 
 	"github.com/clh021/text-parser/tui"
+	tuitodo "github.com/clh021/text-parser/tui-todo"
 )
 
 // 支持启动时显示构建日期和构建版本
@@ -13,6 +14,12 @@ var build = "not set"
 
 func main() {
 	fmt.Printf("Build: %s\n", build)
+
+	if _, err := tuitodo.Run(); err != nil {
+		// if _, err := tea.NewProgram(tui.NewModel(), tea.WithAltScreen()).Run(); err != nil {
+		fmt.Println("Error while running program:", err)
+		os.Exit(1)
+	}
 
 	if _, err := tui.Run(); err != nil {
 		// if _, err := tea.NewProgram(tui.NewModel(), tea.WithAltScreen()).Run(); err != nil {
